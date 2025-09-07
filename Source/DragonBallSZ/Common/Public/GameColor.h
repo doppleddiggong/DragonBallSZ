2f02aa67637951636c9ffe57d7f6fca0ecc9c67b
dopple <kuberin_b@naver.com>
2025-09-07 12:39:14 +0900

    Feat : SightSystem 구축

---
+++ Source/DragonBallSZ/Common/Public/GameColor.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+namespace GameColor
+{
+	static const FColor Pink(255, 0, 108, 255);
+	static const FColor Blue(50, 150, 255, 255);
+	static const FColor Green(50, 255, 50, 255);
+
+}
