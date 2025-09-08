42b493e4ae7b0374ed235d36450a8a4ed8af5e5b
hgh3k <harryh2k@gmail.com>
2025-09-08 18:04:08 +0900

    ADD : 다이나믹 카메라

---
+++ Source/DragonBallSZ/Enviroment/Private/ADynamicCameraActor.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ADynamicCameraActor.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+// Sets default values
+ADynamicCameraActor::ADynamicCameraActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SetRootComponent(SpringArm);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+// Called when the game starts or when spawned
+void ADynamicCameraActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADynamicCameraActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
