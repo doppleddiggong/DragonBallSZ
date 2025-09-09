2ee9822d9cdf080560d05ccccb60f21d0515a640
yeomin-yoon <yeomin1220@gmail.com>
2025-09-09 20:15:19 +0900

    TEST : Testing EnemyFSM

--- Source/DragonBallSZ/Character/Private/UEnemyFSM.cpp
+++ Source/DragonBallSZ/Character/Private/UEnemyFSM.cpp
@@ -2,6 +2,11 @@
 
 
 #include "UEnemyFSM.h"
+
+#include <ThirdParty/ShaderConductor/ShaderConductor/External/DirectXShaderCompiler/include/dxc/DXIL/DxilConstants.h>
+
+#include "AEnemyActor.h"
+#include "APlayerActor.h"
 #include "DragonBallSZ.h"
 
 UEnemyFSM::UEnemyFSM()
@@ -12,44 +17,78 @@ UEnemyFSM::UEnemyFSM()
 void UEnemyFSM::BeginPlay()
 {
 	Super::BeginPlay();
+
+	//Target = Cast<APlayerActor>(GetWorld()->GetFirstPlayerController());
+
+	Itself = Cast<AEnemyActor>(GetOwner());
+
+	TestTarget = GetWorld()->SpawnActor<AActor>(TestFactory, FVector::ZeroVector, FRotator::ZeroRotator);
 }
 
 void UEnemyFSM::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	CurrentTime += DeltaTime;
-	
-	if (CurrentState == EEnemyState::Damaged)	// 피격 시 행동 취소
+
+	if (bDamaged)	// 피격 중에는 선택하지 않는다.
 	{
 		return;
 	}
+	else if (CurrentState == EEnemyState::Damaged)	// 피격 시 행동 취소
+	{
+		void Damaged();
+		return;
+	}
+
+	if (bDefeated)
+	{
+		return;
+	}
+	else if (CurrentState == EEnemyState::EnemyWin)
+	{
+		void EnemyWin();
+		return;
+	}
+	else if (CurrentState == EEnemyState::EnemyLose)
+	{
+		void EnemyLose();
+		return;
+	}
+	
 	if (bActing)	// 행동 수행 중에는 선택하지 않는다.
 	{
 		return;
 	}
+	
 	if (CurrentTime > DecisionTime)	// 시간이 됐으면 행동을 선택한다.
 	{
-		CurrentTime = 0;
+		return;
 	}
+	CurrentTime = 0;
 	
 	// States.Add({EEnemyState::Act, 100.f});	// 가중치 추가
-	CurrentState = SelectWeightedRandomState();
+	if (!bMoving)
+	{
+		CurrentState = SelectWeightedRandomState();
+	}
+	
 	switch (CurrentState)
 	{
 	case EEnemyState::Idle:
 		Idle();
 		break;
 	case EEnemyState::Move:
+		TargetingDestination();
 		Move();
 		break;
-	case EEnemyState::Damaged:
-		Damaged();
+	case EEnemyState::Attack:
+		Attack();
 		break;
-	case EEnemyState::EnemyWin:
-		PlayerDown();
+	case EEnemyState::Charge:
+		Charge();
 		break;
-	case EEnemyState::EnemyLose:
-		EnemyDown();
+	case EEnemyState::Special:
+		Special();
 		break;
 	}
 }
@@ -71,44 +110,131 @@ EEnemyState UEnemyFSM::SelectWeightedRandomState()
 	})].Key;
 }
 
+// Act
 void UEnemyFSM::Idle()
-{
-	PRINTINFO();
-}
-
-
-
-void UEnemyFSM::Damaged()
-{
-	PRINTINFO();
-}
-
-void UEnemyFSM::PlayerDown()
-{
-	PRINTINFO();
-}
-
-void UEnemyFSM::EnemyDown()
 {
 	PRINTINFO();
 }
 
 void UEnemyFSM::Move()
 {
+	// Flying
+	if (bFlying)
+	{
+		MoveSpeed = 1.f;
+		if (IsValid(Itself) == false)
+		{
+			return;
+		}
+		float Distance = FVector::Dist(Destination, Itself->GetActorLocation());
+		AlphaProgress += (GetWorld()->GetDeltaSeconds() * MoveSpeed) / Distance;
+		AlphaProgress = FMath::Clamp(AlphaProgress, 0.0f, 1.0f);
+		
+		float alpha = GetWorld()->GetDeltaSeconds()* MoveSpeed;	//  얼마나 빨리
+		FVector Pa = FMath::Lerp(Itself->GetActorLocation(), CenterControlPoint, AlphaProgress);
+		FVector Pb = FMath::Lerp(CenterControlPoint, Destination, AlphaProgress);
+		FVector P = FMath::Lerp(Pa, Pb, alpha);
+		
+		// 원충돌
+		
+		if (AlphaProgress >= 1.0f)
+		{
+			AlphaProgress = 0;
+			bMoving = false;
+			bActing = true;
+			return;
+		}
+		Itself->SetActorLocation(P, true);
+		return;
+	}
+	
+	// Moving
+	MoveSpeed =500.f;
+	if (IsValid(Itself) == false)
+	{
+		return;
+	}
+	FVector DestinationDirection = Destination - Itself->GetActorLocation();
+	DestinationDirection.Normalize();
+	DestinationDirection.Z = 0;
+	Itself->AddMovementInput(DestinationDirection, MoveSpeed);
+	
+	float dist = FVector::Dist(DestinationDirection, Itself->GetActorLocation());
+	if (dist < 10)
+	{
+		bMoving = false;
+	}
+}
+
+void UEnemyFSM::TargetingDestination()
+{
+	if (bMoving)
+	{
+		return;
+	}
+	
+	// 랜덤한 위치에서 가져오고 싶다.
+	// if (TestTarget)
+	// {
+	// 	Destination = TestTarget->GetActorLocation();
+	// }
+	Destination = FVector(0, 0, 1000);
+
+	// Center Control Point Direction
+	FVector SideVectorFromResult = FVector::CrossProduct(FVector::UpVector, Destination - Itself->GetActorLocation());
+	FVector UpVectorFromResult = FVector::CrossProduct(Destination - Itself->GetActorLocation(), SideVectorFromResult);
+	UpVectorFromResult.Normalize();
+
+	// CenterControlPoint = CenterPoint + Up or Down Vector
+	float Height = Destination.Z - Itself->GetActorLocation().Z;
+	float CurveHeight = FMath::RandRange(Height / -2, Height / 2);
+	CenterControlPoint = (Destination + Itself->GetActorLocation()) * 0.5f + UpVectorFromResult * CurveHeight;
+
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *CenterControlPoint.ToString())
+	bMoving = true;
 }
 
 void UEnemyFSM::Attack()
 {
+	bActing = true;
 	PRINTINFO();
 }
 
 void UEnemyFSM::Charge()
 {
+	bActing = true;
 	PRINTINFO();
 }
 
-void UEnemyFSM::Skill()
+void UEnemyFSM::Special()
 {
+	bActing = true;
 	PRINTINFO();
 }
+
+// Damaged
+void UEnemyFSM::Damaged()
+{
+	bDamaged = true;
+	PRINTINFO();
+}
+
+// Defeated
+void UEnemyFSM::EnemyWin()
+{
+	bDefeated = true;
+	PRINTINFO();
+}
+
+void UEnemyFSM::EnemyLose()
+{
+	bDefeated = true;
+	PRINTINFO();
+}
+
+
+
+
+
+
 
--- Source/DragonBallSZ/Character/Public/UEnemyFSM.h
+++ Source/DragonBallSZ/Character/Public/UEnemyFSM.h
@@ -6,14 +6,16 @@
 #include "GameFramework/Actor.h"
 #include "UEnemyFSM.generated.h"
 
+class AEnemyActor;
+class APlayerActor;
 // Enemy State
 UENUM(BlueprintType)
 enum class EEnemyState : uint8
 {
 	Idle UMETA(DisplayName = "IdleState"),
 	Move UMETA(DisplayName = "MoveState"),
-	Attack UMETA(DisplayName = "AttackState"),
-	Charge UMETA(DisplayName = "ChargeState"),
+	Attack UMETA(DisplayName = "MeleeAttackState"),
+	Charge UMETA(DisplayName = "ChargeKiState"),
 	Special UMETA(DisplayName = "SpecialAttackState"),
 	Damaged UMETA(DisplayName = "DamagedState"),
 	EnemyWin UMETA(DisplayName = "EnemyWinState"),
@@ -23,26 +25,14 @@ enum class EEnemyState : uint8
 // MoveType
 // (Sub State of ActDecision)
 UENUM(BlueprintType)
-enum class AttackType : uint8
-{
-	Approach UMETA(DisplayName = "ApproachMove"),
-	SideStep UMETA(DisplayName = "SideStepMove"),
-
-};
-
-// MoveType
-// (Sub State of ActDecision)
-UENUM(BlueprintType)
 enum class SpecialType : uint8
 {
-	Approach UMETA(DisplayName = "ApproachMove"),
-	SideStep UMETA(DisplayName = "SideStepMove"),
-	Escape UMETA(DisplayName = "EscapeMove"),
+	Kamehameha UMETA(DisplayName = "Kamehameha"),
+	ReleaseKi UMETA(DisplayName = "ReleaseKnockbackKi"),
 };
 
 
-
-UCLASS( Blueprintable, ClassGroup=(DBSZ), meta=(BlueprintSpawnableComponent) )
+UCLASS(Blueprintable, ClassGroup=(DBSZ), meta=(BlueprintSpawnableComponent))
 class DRAGONBALLSZ_API UEnemyFSM : public UActorComponent
 {
 	GENERATED_BODY()
@@ -50,30 +40,56 @@ class DRAGONBALLSZ_API UEnemyFSM : public UActorComponent
 public:
 	UEnemyFSM();
 
+	UPROPERTY()
+	TObjectPtr<APlayerActor> Target;
+	UPROPERTY()
+	TObjectPtr<AEnemyActor> Itself;
+
 protected:
 	virtual void BeginPlay() override;
 
 public:
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
 
 public:
 	// EEnemyState Weights
 	EEnemyState SelectWeightedRandomState();
 	TArray<TPair<EEnemyState, float>> States = {
 		{EEnemyState::Idle, 10.f},
-		{EEnemyState::Move, 200.f}
+		{EEnemyState::Move, 150.f},
+		{EEnemyState::Attack, 10.f},
+		{EEnemyState::Charge, 10.f},
+		{EEnemyState::Special, 0.f},
 	};
 	EEnemyState CurrentState = EEnemyState::Idle;
+	bool bDamaged = false;
+	bool bDefeated = false;
 	bool bActing = false;
 	float CurrentTime = 0;
 	float DecisionTime = 1.5f;
+	
 	void Idle();
-	void Damaged();
-	void PlayerDown();
-	void EnemyDown();
 	void Move();
+	void TargetingDestination();
+	float AlphaProgress = 0;
+	FVector Destination;
+	FVector CenterControlPoint;
+	UPROPERTY(EditAnywhere)
+	bool bMoving;
+	UPROPERTY(EditAnywhere)
+	bool bFlying = false;
+	UPROPERTY(EditAnywhere)
+	float MoveSpeed;
 	void Attack();
 	void Charge();
-	void Skill();
-	
+	void Special();
+	void Damaged();
+	void EnemyWin();
+	void EnemyLose();
+
+	UPROPERTY(EditAnywhere, Category="TestTarget")
+	TSubclassOf<AActor> TestFactory;
+	UPROPERTY()
+	TObjectPtr<AActor> TestTarget;
 };
