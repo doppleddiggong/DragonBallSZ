15b437035f021b979f415ce5a14c2d644873884e
hgh3k <harryh2k@gmail.com>
2025-09-11 16:41:17 +0900

    FIX : 다이나믹 카메라 수정, C++로 옮기기

--- Source/DragonBallSZ/Enviroment/Public/ADynamicCameraActor.h
+++ Source/DragonBallSZ/Enviroment/Public/ADynamicCameraActor.h
@@ -25,8 +25,58 @@ public:
 
 public:
 	UPROPERTY(EditAnywhere)
-	class USpringArmComponent* SpringArm;
+	class USpringArmComponent* SpringArmComp;
 	UPROPERTY(EditAnywhere)
-	class UCameraComponent* Camera;
+	class UCameraComponent* CameraComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class ACharacter* PlayerRef;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class AActor* TargetRef;
 	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CameraDistance = 300;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float TargetDistance = 500;
+	float CurrentDistance;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DeadZonePlayer_X_Min = 0.2f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DeadZonePlayer_X_Max = 0.8f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DeadZonePlayer_Y_Min = 0.1f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DeadZonePlayer_Y_Max = 0.9f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DeadZoneTarget_X_Min = 0.3f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DeadZoneTarget_X_Max = 0.7f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DeadZoneTarget_Y_Min = 0.2f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DeadZoneTarget_Y_Max = 0.8f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CameraResetTime = 5;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void PlayerRotationLock();
+
+	UFUNCTION(BlueprintCallable)
+	void CloseCameraRotation(float DeltaTime);
+
+	UFUNCTION(BlueprintCallable)
+	void ResetCameraLocation(float DeltaTime);
+
+	UFUNCTION()
+	void ResetCameraForwardLocation(float DeltaTime);
+
+	UFUNCTION(BlueprintCallable)
+	void ResetCameraRotation(float DeltaTime);
+
+	bool TargetDeadZoneCheck(const AActor &Target);
 };
