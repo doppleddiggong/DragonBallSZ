befe6992c285e709d68603457d0d31c51dda7fd6
yeomin-yoon <yeomin1220@gmail.com>
2025-09-17 21:18:06 +0900

    ADD : Kamehameha Actor

--- Source/DragonBallSZ/Character/Private/KamehamehaActor.cpp
+++ Source/DragonBallSZ/Character/Private/KamehamehaActor.cpp
@@ -3,8 +3,12 @@
 
 #include "KamehamehaActor.h"
 
+#include "AEnemyActor.h"
+#include "APlayerActor.h"
 #include "DragonBallSZ.h"
 #include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AKamehamehaActor::AKamehamehaActor()
@@ -12,7 +16,16 @@ AKamehamehaActor::AKamehamehaActor()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	ChargeComp = CreateDefaultSubobject<UNiagaraComponent>(TEXT("ChargeComp"));
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp1"));
+	RootComponent = RootComp;
+
+	Kamehameha = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Kamehameha"));
+	Kamehameha->SetupAttachment(RootComponent);
+	Kamehameha->bAutoActivate = false;
+
+	ChargeSphere = CreateDefaultSubobject<UNiagaraComponent>(TEXT("ChargeSphere"));
+	ChargeSphere->SetupAttachment(RootComponent);
+	ChargeSphere->bAutoActivate = true;
 }
 
 // Called when the game starts or when spawned
@@ -20,7 +33,22 @@ void AKamehamehaActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	ChargeComp->Activate();
+	Shooter = Cast<ACharacter>(GetOwner());
+	if (Shooter)
+	{
+		if (Shooter->IsA(APlayerActor::StaticClass())) // Shooter: Player
+		{
+			// Target: Enemy
+			Target = Cast<ACharacter>(UGameplayStatics::GetActorOfClass(GetWorld(), AEnemyActor::StaticClass()));
+			ensureMsgf(Target, TEXT("AEnergyBlastActor: Target 캐스팅 실패! AEnemyActor 필요합니다!"));
+		}
+		else if (Shooter->IsA(AEnemyActor::StaticClass())) // Shooter: Enemy
+		{
+			// Target: Player
+			Target = Cast<APlayerActor>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+			ensureMsgf(Target, TEXT("UEnemyFSM: Target 캐스팅 실패! APlayerActor가 필요합니다!"));
+		}
+	}
 }
 
 // Called every frame
@@ -28,15 +56,32 @@ void AKamehamehaActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (LoopDuration > 0.09f)
+	if (LoopDuration > 0.1f)
 	{
 		LoopDuration -= LoopSpeed;
-		FString distStr = FString::Printf(TEXT("%f"), LoopDuration);
-		PRINTLOG(TEXT("%s"), *distStr);
-		GEngine->AddOnScreenDebugMessage(0, 1, FColor::Cyan, distStr);
-		
-		ChargeComp->SetVariableFloat(FName("LoopDuration"), LoopDuration);
+
+		ChargeSphere->SetVariableFloat(FName("LoopDuration"), LoopDuration);
 	}
 
-	
+	if (Kamehameha && Kamehameha->IsActive())
+	{
+		if (BeamVector.Y < 150)
+		{
+			BeamVector.Y += BeamSpeed;
+			BeamVector.X += BeamSpeed/10;
+			BeamVector.Z += BeamSpeed/10;
+			Kamehameha->SetVariableVec3(FName("BeamVector"), BeamVector);
+		}
+
+		FString distStr = FString::Printf(TEXT("%f"), BeamVector.Y);
+		PRINTLOG(TEXT("%s"), *distStr);
+		GEngine->AddOnScreenDebugMessage(0, 1, FColor::Cyan, distStr);
+	}
+}
+
+void AKamehamehaActor::FireKamehameha()
+{
+	ChargeSphere->DeactivateImmediate();
+
+	Kamehameha->Activate();
 }
