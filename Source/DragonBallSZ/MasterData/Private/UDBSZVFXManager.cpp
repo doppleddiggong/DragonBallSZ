9327fc00caeb06aa38a05d8f8631811f0c65e71e
dopple <kuberin_b@naver.com>
2025-09-18 16:42:01 +0900

    refactor(MasterData): VFX 및 Sound Manager 모듈 위치 변경 및 데이터 구조 개선

---
+++ Source/DragonBallSZ/MasterData/Private/UDBSZVFXManager.cpp
@@ -0,0 +1,81 @@
+﻿// Copyright (c) 2025 Doppleddiggong. All rights reserved. Unauthorized copying, modification, or distribution of this file, via any medium is strictly prohibited. Proprietary and confidential.
+
+#include "UDBSZVFXManager.h"
+
+#include "EAttackPowerType.h"
+#include "DragonBallSZ.h"
+#include "UVFXDataAsset.h"
+#include "NiagaraFunctionLibrary.h"
+
+#define VFX_DATA_PATH TEXT("/Game/CustomContents/MasterData/VFX_Data.VFX_Data")
+
+void UDBSZVFXManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	FSoftObjectPath AssetPath(VFX_DATA_PATH);
+	UVFXDataAsset* LoadedAsset = Cast<UVFXDataAsset>(StaticLoadObject(UVFXDataAsset::StaticClass(), nullptr, *AssetPath.ToString()));
+
+	if (LoadedAsset)
+	{
+		VFXAsset = LoadedAsset;
+		VFXDataMap = VFXAsset->VFXData;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to load VFXDataAsset from path"));
+	}
+}
+
+EVFXType UDBSZVFXManager::ConvertType(EAttackPowerType Type)
+{
+	if( Type == EAttackPowerType::Small )
+		return EVFXType::Hit_Small;
+	else if( Type == EAttackPowerType::Normal )
+		return EVFXType::Hit_Normal;
+	else if( Type == EAttackPowerType::Large )
+		return EVFXType::Hit_Large;
+	else if( Type == EAttackPowerType::Huge )
+		return EVFXType::Hit_Huge;
+
+	return EVFXType::Hit_Small;
+}
+
+bool UDBSZVFXManager::GetVFXData(EVFXType Type, FVFXData& Out) const
+{
+	if (const FVFXData* Found = VFXDataMap.Find(Type))
+	{
+		Out = *Found;
+		return true;
+	}
+
+	PRINTLOG(TEXT("DataGetFail : %s"), *UEnum::GetValueAsString(Type) );
+	return false;
+}
+
+void UDBSZVFXManager::ShowVFXAttackType( EAttackPowerType Type,
+	FVector Location, FRotator Rotator, FVector Scale)
+{
+	auto ConvertVFX= ConvertType(Type);
+	this->ShowVFX(ConvertVFX, Location, Rotator, Scale);
+}
+
+void UDBSZVFXManager::ShowVFX( EVFXType Type,
+	FVector Location, FRotator Rotator, FVector Scale)
+{
+	FVFXData Params;
+	if (!this->GetVFXData(Type, Params))
+		return;
+
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+		GetWorld(),
+		Params.VFXAsset,
+		Location + Params.LocationOffset,
+		Rotator + Params.RotatorOffset,
+		Scale * Params.Scale,
+		true,
+		true,
+		ENCPoolMethod::None,
+		true
+	);
+}
--- Source/DragonBallSZ/MasterData/Public/UDBSZVFXManager.h
+++ Source/DragonBallSZ/MasterData/Public/UDBSZVFXManager.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Core/Macro.h"
+#include "EAttackPowerType.h"
 #include "EVFXType.h"
+#include "UVFXDataAsset.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "UDBSZVFXManager.generated.h"
 
@@ -19,12 +21,21 @@ public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 
 	UFUNCTION(BlueprintCallable, Category="VFX")
+	EVFXType ConvertType(EAttackPowerType Type);
+
+	UFUNCTION(BlueprintCallable, Category="VFX")
+	bool GetVFXData(EVFXType Type, FVFXData& Out) const;
+
+	UFUNCTION(BlueprintCallable, Category="VFX")
+	void ShowVFXAttackType(EAttackPowerType Type, FVector Location, FRotator Rotator, FVector Scale);
+
+	UFUNCTION(BlueprintCallable, Category="VFX")
 	void ShowVFX( EVFXType Type, FVector Location, FRotator Rotator, FVector Scale);
 	
 private:
 	UPROPERTY(EditDefaultsOnly, Category = "VFX")
-	TObjectPtr<class UVFXData> VFXAsset;
+	TObjectPtr<class UVFXDataAsset> VFXAsset;
 	
 	UPROPERTY(EditAnywhere, Category = "VFX")
-	TMap<EVFXType, TObjectPtr<class UNiagaraSystem>> VFXData; 
+	TMap<EVFXType, FVFXData> VFXDataMap; 
 };
